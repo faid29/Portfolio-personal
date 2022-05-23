6da08c38b0ba7ed751c7ffe004effbef9da6b3b1
faid29 <rubensarco_7@hotmail.com>
2022-05-23 00:26:47 -0300

    actualizacion cambios en el boton shared

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -57,35 +57,6 @@ export class MainComponent implements OnInit {
       'titulo1':'Hola, soy',
       'subtitulo':'Full Stack Developer Jr'
     }
-  
-
-  dataShare=[
-    {
-      'titulo':'Linkedln',
-      'url':'https://img.icons8.com/fluency/48/000000/linkedin.png',
-      'a':'https://www.linkedin.com/sharing/share-offsite/?url=https://www.linkedin.com/in/rubenisarco/',
-    },
-    {
-      'tiulo':'Git',
-      'url':'https://img.icons8.com/fluency/48/000000/github.png',
-      'a':'https://www.linkedin.com/sharing/share-offsite/?url=https://github.com/faid29/',
-    },
-    { 
-    'titulo':'Twitter',
-    'url':'https://img.icons8.com/fluency/344/twitter.png',
-    'a':'https://twitter.com/intent/tweet?text=[text]&url=[https://twitter.com/ruben_sarco]&hashtags=[hashtag]"',
-    },
-    {
-      'titulo':'Whatsapp',
-      'url':'https://img.icons8.com/fluency/48/000000/whatsapp.png',
-      'a':'https://api.whatsapp.com/send?text=[Hola soy Rubén Sarco, https://portafolio-rubensarco.web.app estás compartiendo mi portafolio]',
-    },
-    {
-      'titulo':'Telegram',
-      'url':'https://img.icons8.com/color/48/000000/telegram-app--v3.png',
-      'a':'https://telegram.me/share/url?url=https://portafolio-rubensarco.web.app&text=Hola comparte mi portafolio, gracias',
-    }
-  ]
 
   @ViewChild("formPersona") formPersona!: FormPersonaComponent;
   @Input() personas: Persona[] = [];
